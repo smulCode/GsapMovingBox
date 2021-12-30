f5fd031b5c6d0718757fb3508a170c7c9f3e430d
Wesley Smulders <67417862+smulCode@users.noreply.github.com>
2021-12-30 10:39:05 +0100

    timeline color

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,8 @@
 // import {example} from "./modules/example.js"
+
+
+const MasterTimeline = gsap.timeline({defaults: {durations:2,}})
+MasterTimeline.
+to(".main", {y:"-100%", x:0, opacity:1 , ease:"expo.out",stagger:0.25,})
+.to(".box-container", {backgroundColor:"orange", ease:"bounce"})
+
