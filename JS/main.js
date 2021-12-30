52da7a099e8e50e66ffc974ac876bce78f07c1bf
Wesley Smulders <67417862+smulCode@users.noreply.github.com>
2021-12-30 16:18:38 +0100

    full animation

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,9 @@
 // import {example} from "./modules/example.js"
-const slideContainer = document.querySelector(".slide-container");
+const slideContainer = document.querySelector(".slide-track");
 const slides = Array.from(slideContainer.children);
 
 const slideWidth = slides[0].getBoundingClientRect().width;
+console.log(slideWidth);
 
 const setSlidePosition = (slide, index) => {
   slide.style.left = slideWidth * index + "px";
@@ -10,17 +11,18 @@ const setSlidePosition = (slide, index) => {
 
 slides.forEach(setSlidePosition);
 
-const MasterTimeline = gsap.timeline({defaults: {duration:1,}})
-MasterTimeline.
-to(".main", {y:"-100%", x:0, opacity:1 , ease:"expo.out",stagger:0.25,})
-.to(".slide-container", {backgroundColor:"orange", ease:"bounce" })
-.to(".slide", {opacity:1, ease:"power2.in"})
-
-// ,onComplete: () => slideTimeline.play()
+const MasterTimeline = gsap.timeline({defaults: {duration:1, smoothChildTiming:true,}})
+MasterTimeline
+.to(".main", {y:"-100%", x:0, opacity:1 , ease:"expo.out",stagger:0.25, delay:1})
+.to(".slide-container", {backgroundColor:"#aaa", ease:"none" })
+.to(".slide-track", {opacity:0,scale:0, ease:"power4.out",})
+.to(".slide-track", {opacity:1,scale:1, ease:"power4.out",})
+.to(".slide", {y:0,opacity:1, ease:"power4.out",onComplete: () => slideTimeline.restart()},"<-0.5")
+.to(".main", {y:"-200%", x:0, opacity:0 , ease:"power4.in",stagger:0.25,delay:4,onComplete: () => MasterTimeline.restart()},"<5.5")
 
 let slideTimeline = gsap.timeline({defaults: {duration:1.5}}).pause();
 
 slideTimeline
-.to(".slide", {x:-(slideWidth), ease:"expo.inOut", stagger:0.25})
+.to(".slide", {x:-(slideWidth), ease:"expo.inOut", stagger:0.25 ,delay:1,})
 .to(".slide", {x:-(slideWidth * 2), ease:"expo.inOut",stagger:0.25})
 .to(".slide", {x:0, ease:"expo.inOut", stagger:-0.25, delay:1, })
