0978589bb620c7c96d927749c303485043f324dd
Wesley Smulders <67417862+smulCode@users.noreply.github.com>
2021-12-30 11:15:53 +0100

    set

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,18 @@
 // import {example} from "./modules/example.js"
+const slideContainer = document.querySelector(".slide-container");
+const slides = Array.from(slideContainer.children);
 
+const slideWidth = slides[0].getBoundingClientRect().width;
 
-const MasterTimeline = gsap.timeline({defaults: {durations:2,}})
+const setSlidePosition = (slide, index) => {
+  slide.style.left = slideWidth * index + "px";
+};
+
+slides.forEach(setSlidePosition);
+
+const MasterTimeline = gsap.timeline({defaults: {durations:1,}})
 MasterTimeline.
 to(".main", {y:"-100%", x:0, opacity:1 , ease:"expo.out",stagger:0.25,})
-.to(".box-container", {backgroundColor:"orange", ease:"bounce"})
+.to(".slide-container", {backgroundColor:"orange", ease:"bounce"})
+.to(".slide", {x:"-100px", opacity:0.5, backgroundColor:"white", ease:"expo",})
 
